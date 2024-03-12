4dc1d2eb4cc0c020d52c04be3c85f36c98bfa3eb
kuldip lohare <162694586+kuldipl78@users.noreply.github.com>
2024-03-12 19:06:23 +0530

    Update app.js

--- app.js
+++ app.js
@@ -179,3 +179,69 @@ app.delete('/todos/:todoId/', async (request, response) => {
 })
 
 module.exports = app
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import {format, compareAsc} from 'date-fns'
+const express = require('express')
+const {open} = require('sqlite')
+const sqlite3 = require('sqlite3')
+const path = require('path')
+
+const dbPath = path.join(__dirname, 'todoApplication.db')
+
+const app = express()
+
+app.use(express.json())
+
+let db = null
+
+const initializeDbAndServer = async () => {
+  try {
+    database = await open({
+      filename: dbPath,
+      driver: sqlite3.Database,
+    })
+
+    app.listen(3000, () =>
+      console.log('Server Running at http://localhost:3000/'),
+    )
+  } catch (error) {
+    console.log(`DB Error: ${error.message}`)
+    process.exit(1)
+  }
+}
+
+initializeDbAndServer()
+
+const hasPriorityAndStatusProperties = requestQuery => {
+  return (
+    requestQuery.priority !== undefined && requestQuery.status !== undefined
+  )
+}
+
+const hasPriorityProperty = requestQuery => {
+  return requestQuery.priority !== undefined
+}
+
+const hasStatusProperty = requestQuery => {
+  return requestQuery.status !== undefined
+}
+
